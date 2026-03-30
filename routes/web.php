737655a71d5363d5298fd8c789c802e8b601c13f
BudiWinarno <budiwinarno2@gmail.com>
2026-03-30 13:33:15 +0700

    test ci/cd deploy.sh

--- routes/web.php
+++ routes/web.php
@@ -6,24 +6,24 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-// Route::get('/', function () {
-//     return Inertia::render('Welcome', [
-//         'canLogin' => Route::has('login'),
-//         'canRegister' => Route::has('register'),
-//         'laravelVersion' => Application::VERSION,
-//         'phpVersion' => PHP_VERSION,
-//     ]);
-// });
-
 Route::get('/', function () {
-    // Cek apakah user sudah login atau belum
-    if (Auth::check()) {
-        return redirect('/dashboard');
-    }
-
-    // Jika belum login, arahkan ke halaman login
-    return redirect('/login');
+    return Inertia::render('Welcome', [
+        'canLogin' => Route::has('login'),
+        'canRegister' => Route::has('register'),
+        'laravelVersion' => Application::VERSION,
+        'phpVersion' => PHP_VERSION,
+    ]);
 });
+
+// Route::get('/', function () {
+//     // Cek apakah user sudah login atau belum
+//     if (Auth::check()) {
+//         return redirect('/dashboard');
+//     }
+
+//     // Jika belum login, arahkan ke halaman login
+//     return redirect('/login');
+// });
 
 Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
